c9c1ee888ad304be728bc01ad78154facaaa032f
Felipe Barraza Vega <felipe.barraza.vega@gmail.com>
2022-11-04 15:51:47 -0300

    mejoramiento de interfaz en crm

--- ui_crm/src/components/quotations/Home.js
+++ ui_crm/src/components/quotations/Home.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import  api from '../../api/endpoints'
 import { Table, Button, Modal, 
-          Typography, Card, Descriptions,
+          Tooltip, Card, Descriptions,
           Row, Col } from 'antd'
-import { UserOutlined, UnorderedListOutlined } from '@ant-design/icons'
+import { SendOutlined, CloudDownloadOutlined, EyeFilled } from '@ant-design/icons'
 
 const Home = () => {
 
     const [listQuotations, setListQuotations] = useState([])
     
     async function getData(){
-      const rq = await api.quotation.list().then((x)=> {
-        console.log(x)
+      const rq = await api.quotation.list().then((x)=> {        
         setListQuotations(x.data.results)
       })
       return rq
@@ -19,22 +18,31 @@ const Home = () => {
     
     function modalRetrieveClient(client) {
       Modal.info({ 
-        width: 500,
+        width: 750,
+        okText: 'Volver',
         title: client.name_enterprise,
-        content: <Typography.Paragraph>
-                    <b>NOMBRE CONTACTO:</b> {client.name_contact}<br/>
-                    <b>TELEFONO:</b> {client.phone_contact}<br/>
-                    <b>CORREO:</b> {client.mail_contact}<br/>
-                    <b>NOMBE EMPRESA:</b> {client.name_enterprise}<br/>
-                    <b>DIRECCIÓN EMPRESA:</b> {client.address_enterprise}<br/>
-          </Typography.Paragraph>
+        content: <Descriptions bordered layout='horizontal' style={{marginTop:'50px'}} size='middle'>
+                  <Descriptions.Item label='Nombre contacto' span={3}>
+                    <b>{client.name_contact}</b>
+                  </Descriptions.Item>            
+                  <Descriptions.Item label='Teléfono' span={3}>
+                    <b>{client.phone_contact}</b>
+                  </Descriptions.Item>
+                  <Descriptions.Item label='Email' span={3}>
+                    <b>{client.mail_contact}</b>
+                  </Descriptions.Item>
+                  <Descriptions.Item label='Nombre empresa' span={3}>
+                    <b>{client.name_enterprise}</b>
+                  </Descriptions.Item>
+          </Descriptions>
       })
     }
     
     function modalDataWells(wells) {
       Modal.info({
-        width: 1000,
-        title: 'POZOS',
+        width: 1000,  
+        icon:<></>,    
+        okText:'Volver',  
         content: <Row>
           {wells.map((x)=> {
             return(<Col span={12}><Card title={x.name}>
@@ -53,50 +61,67 @@ const Home = () => {
       })
     }
 
-    console.log(listQuotations)
 
     useEffect(() => {
       getData()
     }, [])
 
-    return(<Table
-        columns = {[
+    return(<Table bordered
+        columns = {[          
           {
-            title:'URL Cotización',
+            title: 'Datos de contacto',
             render: (x)=> <>
-              <a href={`https://smarthydro.cl/dgaform/external_client/${x.uuid}`} target="_blank">{x.uuid}</a>
+              {x.external_client.name_enterprise.length < 8 ? 
+               <Button 
+               type='primary'
+               ghost
+               style={{textAlign:'left'}}
+               block
+               icon={<EyeFilled style={{marginRight:'5px'}} />}
+               onClick={()=>modalRetrieveClient(x.external_client)}>
+                  {x.external_client.name_enterprise}                                 
+             </Button>:<Tooltip color={'#f50'} title={x.external_client.name_enterprise}><Button 
+               type='primary'
+               ghost
+               style={{textAlign:'left'}}
+               block
+               icon={<EyeFilled style={{marginRight:'5px'}} />}
+               onClick={()=>modalRetrieveClient(x.external_client)}>
+                  {x.external_client.name_enterprise.slice(0,20)}...                                 
+             </Button></Tooltip>
+              }               
             </>
-          }, 
+          },
+          {
+            title: 'Pozos',
+            render: (x) => <>
+              <Button 
+                type='primary'
+                ghost
+                block
+                icon={<EyeFilled />}
+                onClick={()=> modalDataWells(x.wells)}>
+                 {x.wells.length} {x.wells.length === 1 ? 'Pozo':'Pozos'}
+              </Button>
+            </>
+          },
           {
             title: 'Fecha creación',
-            render: (x) => <>{x.created.slice(0,10)} - {x.created.slice(11,16)}</>
-          },
-          {
-            title: 'Cliente',
-            render: (x)=> <>
-              <Button icon={<UserOutlined />}
-                type='primary'
-                onClick={()=>modalRetrieveClient(x.external_client)}>
-                  {x.external_client.name_enterprise}
-              </Button>
-            </>
-          },
-          {
-            title: 'POZOS',
-            render: (x) => <>
-              <Button icon={<UnorderedListOutlined />} type='primary'
-                onClick={()=> modalDataWells(x.wells)}>
-                POZOS({x.wells.length})
-              </Button>
-            </>
-          },
-          {
-            title: 'Está aprobada?',
+            render: (x) => {
+              var date = new Date(x.created)
+              var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour:'numeric', minute:'numeric' }
+             return (<>{date.toLocaleDateString('es-ES', options).toUpperCase()}</>)
+            }
+          },                    
+          {            
             render: (x)=> {
               if(x.is_approved){
                 return(<Button  danger>CANCELAR APROBACIÓN</Button>)
               } else {
-                return(<Button >APROBAR</Button>)
+                return(<>
+                  <Button type='primary' icon={<SendOutlined />}>Revisado / Cotizar</Button>
+                  <Button type='primary' icon={<CloudDownloadOutlined/>} style={{backgroundColor:'#389e0d', borderColor:'#389e0d', marginLeft:'10px'}}>Descargar reporte</Button>                  
+                  </>)
               }
             }
           }
